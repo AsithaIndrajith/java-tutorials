10da5dd6c1491e477470ab6d7823b360dea908d7
AsithaIndrajith <asithaindrajithk9@gmail.com>
2018-01-05 23:02:25 +0530

    Update regularExpression.java

--- regularExpression.java
+++ regularExpression.java
@@ -8,13 +8,14 @@ public class regularExpression{
 	public static void main( String[] args){
 		// String to be scanned to find the pattern.
 		String line = "I am NOT DOG right?";
-		String pattern = "(DOG)";//pattern that need to search
+		String pattern = "(DOG)(WE)";//pattern that need to search
 		// Create a Pattern object
 		Pattern r = Pattern.compile(pattern);
 		// Now create matcher object.
 		Matcher m = r.matcher(line);
 		if (m.find( )) {//m is a matching object,m.find do search is there a pattern in the line
 			System.out.println("Found value: " + m.group(0) );//if true we access that by group method
+			System.out.println("Found value: " + m.group(1) );
 		} 
 		else {
 			System.out.println("NO MATCH");
